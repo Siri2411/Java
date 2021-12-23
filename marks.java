0642c897cf2513b6dac04b2cdcee6a41b9300e18
Siri2411 <83855091+Siri2411@users.noreply.github.com>
2021-12-23 10:17:48 +0530

    Add files via upload

---
+++ marks.java
@@ -0,0 +1,15 @@
+package CIE;
+	import java.util.Scanner;
+	public class Internals extends CIE.Student
+	{
+		Scanner scan = new Scanner(System.in);
+		public int ciem[] = new int[5];
+		
+		public void accept()
+	{
+		for(i=0; i<5; i++)
+	{
+		System.out.println("Enter cie marks of subject "+(i+1));
+		ciem[i] = scan.nextInt();
+	}
+}
