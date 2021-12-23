b4792f2a299c5a1da25cdff0ed68818716380b32
Siri2411 <83855091+Siri2411@users.noreply.github.com>
2021-12-23 09:36:51 +0530

    Add files via upload

---
+++ pac.java
@@ -0,0 +1,76 @@
+package pack;
+import p1.A;
+import p1.B;
+import p1.p11.C;
+import p2.D;
+
+
+ public class packtest{
+
+
+    public static void main(String args[])
+    {
+
+
+          A a1=new A();
+          B b1=new B();
+          D d1=new D();
+          C c1=new C();
+
+           a1.dispA();
+           b1.dispB();
+           c1.dispC();
+           d1.dispD();
+      
+    }
+ } package pack;
+import p1.A;
+import p1.B;
+import p1.p11.C;
+import p2.D;
+
+
+ public class packtest{
+
+
+    public static void main(String args[])
+    {
+
+
+          A a1=new A();
+          B b1=new B();
+          D d1=new D();
+          C c1=new C();
+
+           a1.dispA();
+           b1.dispB();
+           c1.dispC();
+           d1.dispD();
+      
+    }
+ } package pack;
+import p1.A;
+import p1.B;
+import p1.p11.C;
+import p2.D;
+
+
+ public class packtest{
+
+
+    public static void main(String args[])
+    {
+
+
+          A a1=new A();
+          B b1=new B();
+          D d1=new D();
+          C c1=new C();
+
+           a1.dispA();
+           b1.dispB();
+           c1.dispC();
+           d1.dispD();
+      
+    }
+ } 
