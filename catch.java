f062e155636f42b535a306f4f48920317d15f7b0
Siri2411 <83855091+Siri2411@users.noreply.github.com>
2021-12-30 10:08:45 +0530

    Add files via upload

---
+++ catch.java
@@ -0,0 +1,24 @@
+class multicatch
+{
+public static void main(String args[])
+{
+
+try
+{
+int a=args.length;
+
+System.out.println("a="+a);
+int b=42/a;
+int c[]={1};
+c[42]=99;
+}
+catch(ArithmeticException e)
+{
+System.out.println("division by zero"+e);
+}
+catch(ArrayIndexOutOfBoundsException e)
+{
+System.out.println("array index out of bound"+e);
+}
+System.out.println("after catch statement");
+}}
